360a4b20fdca40b7bfc6d00ca6e1f3bb8f463c90
varunbhalla19 <varunbhalla19@gmail.com>
2022-12-24 14:12:24 +0530

    added extension for theme colors.

---
+++ SwiftyCrypto/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  SwiftyCrypto
+//
+//  Created by varunbhalla19 on 24/12/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent: Color = Color("AccentColor")
+    let background: Color = Color("BackgroundColor")
+    let green: Color = Color("GreenColor")
+    let red: Color = Color("RedColor")
+    let secondaryText: Color = Color("SecondaryTextColor")
+}

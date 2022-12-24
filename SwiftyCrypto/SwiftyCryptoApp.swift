c6991d359b8f15a32ffdf72ca3a4eb1e5e7f38fc
varunbhalla19 <varunbhalla19@gmail.com>
2022-12-24 14:11:56 +0530

    initial commit with imported assets.

---
+++ SwiftyCrypto/SwiftyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCryptoApp.swift
+//  SwiftyCrypto
+//
+//  Created by varunbhalla19 on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

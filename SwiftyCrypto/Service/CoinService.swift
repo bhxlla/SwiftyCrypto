fd8dd356051c857749b780bb9240dd1bcf249f38
varunbhalla19 <varunbhalla19@gmail.com>
2022-12-30 23:47:56 +0530

    added CoinService class to fetch coin data from urlSession publisher.

---
+++ SwiftyCrypto/Service/CoinService.swift
@@ -0,0 +1,44 @@
+//
+//  CoinService.swift
+//  SwiftyCrypto
+//
+//  Created by varunbhalla19 on 30/12/22.
+//
+
+import Foundation
+import Combine
+
+class CoinService {
+    
+    @Published var allCoins: [Coin] = []
+    var cancellable: AnyCancellable?
+    private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+    
+    private let decoder = JSONDecoder()
+    
+    init(){
+        getCoins()
+    }
+    
+    private func getCoins(){
+        guard let url = URL(string: urlString) else { return }
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data: Data, response: URLResponse) in
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }.decode(type: [Coin].self, decoder: decoder)
+            .sink { [weak self] result in
+                if case let .failure(err) = result {
+                    print(err.localizedDescription)
+                }
+                self?.cancellable?.cancel()
+            } receiveValue: { [weak self] coins in
+                self?.allCoins = coins
+            }
+
+    }
+    
+}

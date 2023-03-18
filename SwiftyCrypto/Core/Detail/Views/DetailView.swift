a3cfd601aecf1f9be50e27fd277959ddb155ee2d
varunbhalla19 <varunbhalla19@gmail.com>
2023-03-18 17:11:01 +0530

    Added initial DetailView

---
+++ SwiftyCrypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailView.swift
+//  SwiftyCrypto
+//
+//  Created by varunbhalla19 on 12/03/23.
+//
+
+import SwiftUI
+
+struct DetailScreen: View {
+    @Binding var coin: Coin?
+    
+    init(coin: Binding<Coin?>) {
+        self._coin = coin
+    }
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: Coin
+    
+    var body: some View {
+        VStack {
+            Text(coin.name)
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(coin: dev.coin)
+    }
+}
